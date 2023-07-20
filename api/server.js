812c4195cc04de857b5022b91cd107425a34a900
Le Tran Quy Nhan <nhan.le@asnet.com.vn>
2023-07-20 17:45:08 +0700

    fix: update congig json

--- api/server.js
+++ api/server.js
@@ -2,19 +2,28 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 
+const jsonServer = require("json-server");
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-const rules = jsonServer.rewriter({
-  "/books/*": "/$1",
-});
+const rules = jsonServer.rewriter();
 
 // /!\ Bind the router db to the app
 app.db = router.db;
 
+app.use(middlewares);
+app.use((req, res, next) => {
+  if (isAuthorized(req)) {
+    // add your authorization logic here
+    next(); // continue to JSON Server router
+  } else {
+    res.sendStatus(401);
+  }
+});
 // You must apply the auth middleware before the router
 app.use(cors());
 app.use(rules);
 app.use(auth);
-app.use("/books", router);
+app.use(router);
 app.listen(3000);
