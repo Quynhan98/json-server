0eef0d0f13b8081dc1f4a171c27291465cdc4345
Le Tran Quy Nhan <nhan.le@asnet.com.vn>
2023-07-20 17:49:55 +0700

    fix: update server json

--- api/server.js
+++ api/server.js
@@ -2,15 +2,24 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 
-const jsonServer = require("json-server");
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 const rules = jsonServer.rewriter("db.json");
 
 // /!\ Bind the router db to the app
 app.db = router.db;
 
+app.use(middlewares);
+app.use((req, res, next) => {
+  if (isAuthorized(req)) {
+    // add your authorization logic here
+    next(); // continue to JSON Server router
+  } else {
+    res.sendStatus(401);
+  }
+});
 // You must apply the auth middleware before the router
 app.use(cors());
 app.use(rules);
