5fc62e2b358f2ec8620d256a0cc912888d4355bf
Le Tran Quy Nhan <nhan.le@asnet.com.vn>
2023-07-20 15:59:01 +0700

    fix: update server

--- api/server.js
+++ api/server.js
@@ -1,22 +1,16 @@
-// See https://github.com/typicode/json-server#module
-// const jsonServer = require("json-server");
-const jsonServerAuth = require("json-server-auth");
-const server = jsonServerAuth.create();
-const router = jsonServerAuth.router("db.json");
-const middlewares = jsonServerAuth.defaults();
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 
-server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  jsonServerAuth.rewriter({
-    "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
-  })
-);
-server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
-});
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+// /!\ Bind the router db to the app
+app.db = router.db;
+
+// You must apply the auth middleware before the router
+app.use(auth);
+app.use(router);
+app.listen(3000);
 
 // Export the Server API
 module.exports = server;
