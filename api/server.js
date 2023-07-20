a9c67afe69aff7748e54b622cb3166e8644e9034
Le Tran Quy Nhan <nhan.le@asnet.com.vn>
2023-07-20 17:17:22 +0700

    Update config

--- api/server.js
+++ api/server.js
@@ -1,17 +1,20 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
+const path = require("path");
 
 const app = jsonServer.create();
-const router = jsonServer.router("db.json");
-const filePath = path.join("/tmp", "db.json");
-fs.writeFileSync(filePath, JSON.stringify(data));
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 
 // /!\ Bind the router db to the app
 app.db = router.db;
 
 // You must apply the auth middleware before the router
+app.use(middlewares);
 app.use(cors());
 app.use(auth);
 app.use(router);
-app.listen(3000);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
